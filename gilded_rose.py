eb898e8a1d297a254302764e9d0552aa96d8d56a
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:17:02 +0200

    Removed variable isNormalItem by dealing with special cases first.

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,17 +7,16 @@ class GildedRose:
 
             isAgedBrie = item.name == "Aged Brie"
             isBackstagePasses = item.name == "Backstage passes to a TAFKAL80ETC concert"
-            isNormalItem = not isAgedBrie and not isBackstagePasses
 
-            if isNormalItem:
-                item.quality = item.quality - 1
-            elif isAgedBrie or isBackstagePasses:
+            if isAgedBrie or isBackstagePasses:
                 if item.sell_in <= 5:
                     item.quality = item.quality + 3
                 elif item.sell_in <= 10:
                     item.quality = item.quality + 2
                 else:
                     item.quality = item.quality + 1
+            else:
+                item.quality = item.quality - 1
 
             item.sell_in = item.sell_in - 1
             if item.quality > 50:
@@ -26,7 +25,7 @@ class GildedRose:
             if item.sell_in < 0:
                 if isBackstagePasses:
                     item.quality = 0
-                elif isNormalItem and item.quality > 0:
+                elif not isAgedBrie and item.quality > 0:
                     item.quality = item.quality - 1
                 else:
                     if item.quality < 50:
