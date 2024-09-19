357bc68d79b9c0020a471a6db1b9df9cfe2fa6af
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:37:05 +0200

    Simplified and fixed conditions for Aged Brie and Backstage Passes.

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,6 +2,7 @@ class GildedRose:
     @staticmethod
     def update_quality(items):
         for item in items:
+            # Legendary item
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
@@ -10,25 +11,24 @@ class GildedRose:
 
             item.sell_in -= 1
 
-            if is_aged_brie or is_backstage_passes:
-                if item.sell_in <= 5:
+            # Special items time-wise
+            if is_backstage_passes:
+                if item.sell_in < 0:
+                    item.quality = 0
+                elif item.sell_in <= 5:
                     item.quality = item.quality + 3
                 elif item.sell_in <= 10:
                     item.quality = item.quality + 2
                 else:
                     item.quality = item.quality + 1
-            else:
+            elif is_aged_brie:
+                item.quality = item.quality + 1
+            else:  # Normal item
                 item.quality = item.quality - 1
 
-            if item.sell_in < 0:
-                if is_backstage_passes:
-                    item.quality = 0
-                elif not is_aged_brie and item.quality > 0:
+            if item.sell_in <= 0:
+                if not is_aged_brie and item.quality > 0:
                     item.quality = item.quality - 1
-                else:
-                    if item.sell_in <= 0:
-                        item.quality = 0
-                        # of for.
 
             if item.quality > 50:
                 item.quality = 50
