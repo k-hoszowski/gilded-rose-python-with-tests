b414a140d2af126ec52767248c02c28d92e8c65b
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:24:52 +0200

    Renamed variables to be more pythonic.

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,12 +5,12 @@ class GildedRose:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            isAgedBrie = item.name == "Aged Brie"
-            isBackstagePasses = item.name == "Backstage passes to a TAFKAL80ETC concert"
+            is_aged_brie = item.name == "Aged Brie"
+            is_backstage_passes = item.name == "Backstage passes to a TAFKAL80ETC concert"
 
             item.sell_in -= 1
 
-            if isAgedBrie or isBackstagePasses:
+            if is_aged_brie or is_backstage_passes:
                 if item.sell_in <= 5:
                     item.quality = item.quality + 3
                 elif item.sell_in <= 10:
@@ -21,9 +21,9 @@ class GildedRose:
                 item.quality = item.quality - 1
 
             if item.sell_in < 0:
-                if isBackstagePasses:
+                if is_backstage_passes:
                     item.quality = 0
-                elif not isAgedBrie and item.quality > 0:
+                elif not is_aged_brie and item.quality > 0:
                     item.quality = item.quality - 1
                 else:
                     if item.sell_in <= 0:
