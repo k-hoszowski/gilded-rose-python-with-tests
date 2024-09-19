036e356a12be5b52ad280d3756fe96b9aaa59d63
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:24:16 +0200

    Moved statements.

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,6 +8,8 @@ class GildedRose:
             isAgedBrie = item.name == "Aged Brie"
             isBackstagePasses = item.name == "Backstage passes to a TAFKAL80ETC concert"
 
+            item.sell_in -= 1
+
             if isAgedBrie or isBackstagePasses:
                 if item.sell_in <= 5:
                     item.quality = item.quality + 3
@@ -18,8 +20,6 @@ class GildedRose:
             else:
                 item.quality = item.quality - 1
 
-            item.sell_in -= 1
-
             if item.sell_in < 0:
                 if isBackstagePasses:
                     item.quality = 0
@@ -29,6 +29,7 @@ class GildedRose:
                     if item.sell_in <= 0:
                         item.quality = 0
                         # of for.
+
             if item.quality > 50:
                 item.quality = 50
 
