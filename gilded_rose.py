10437514ed124d6673fb385dc8a2d80ae556ed30
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:38:32 +0200

    Extracted quality constraint check.

--- gilded_rose.py
+++ gilded_rose.py
@@ -27,8 +27,11 @@ class GildedRose:
                 item.quality = item.quality - 1
 
             if item.sell_in <= 0:
-                if not is_aged_brie and item.quality > 0:
+                if not is_aged_brie:
                     item.quality = item.quality - 1
+
+            if item.quality < 0:
+                item.quality = 0
 
             if item.quality > 50:
                 item.quality = 50
