2f859de5a66fb7323b5c033c6baee1c62057e7a5
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 00:23:47 +0200

    Moved conditions.

--- gilded_rose.py
+++ gilded_rose.py
@@ -18,9 +18,7 @@ class GildedRose:
             else:
                 item.quality = item.quality - 1
 
-            item.sell_in = item.sell_in - 1
-            if item.quality > 50:
-                item.quality = 50
+            item.sell_in -= 1
 
             if item.sell_in < 0:
                 if isBackstagePasses:
@@ -31,5 +29,7 @@ class GildedRose:
                     if item.sell_in <= 0:
                         item.quality = 0
                         # of for.
+            if item.quality > 50:
+                item.quality = 50
 
         return items
