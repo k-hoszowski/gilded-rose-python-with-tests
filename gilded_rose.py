98ee41dc5c71c8d64277db602c342c1da5a57f78
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-19 23:50:26 +0200

    Merged conditions.

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,22 +2,21 @@ class GildedRose:
     @staticmethod
     def update_quality(items):
         for i in range(0, len(items)):
-            isAgedBrie = "Aged Brie" == items[i].name
-            isBackstagePasses = "Backstage passes to a TAFKAL80ETC concert" == items[i].name
-            isSulfuras = "Sulfuras, Hand of Ragnaros" == items[i].name
+            isAgedBrie = items[i].name == "Aged Brie"
+            isBackstagePasses = items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+            isSulfuras = items[i].name == "Sulfuras, Hand of Ragnaros"
 
-            if not isAgedBrie and not isBackstagePasses and items[i].quality > 0 and not isSulfuras:
+            if not isAgedBrie and not isBackstagePasses and not isSulfuras and items[i].quality > 0:
                 items[i].quality = items[i].quality - 1
             else:
                 if items[i].quality < 50:
                     items[i].quality = items[i].quality + 1
                     if "Aged Brie" == items[i].name:
                         if items[i].sell_in < 6:
+                            items[i].quality = items[i].quality + 2
+                        elif items[i].sell_in < 11:
                             items[i].quality = items[i].quality + 1
-                    # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                    if "Aged Brie" == items[i].name:
-                        if items[i].sell_in < 11:
-                            items[i].quality = items[i].quality + 1
+
                     if isBackstagePasses:
                         if items[i].sell_in < 11:
                             # See revision number 2394 on SVN.
@@ -36,7 +35,7 @@ class GildedRose:
             if items[i].sell_in < 0:
                 if isBackstagePasses:
                     items[i].quality = 0
-                elif not isAgedBrie and items[i].quality > 0 and not isSulfuras:
+                elif not isAgedBrie and not isSulfuras and items[i].quality > 0:
                     items[i].quality = items[i].quality - 1
                 else:
                     if items[i].quality < 50:
