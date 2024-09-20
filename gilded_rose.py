fa41231057df2b23e250cc9e5ff3f59950ff32ef
Krzysztof Hoszowski <k-hoszowski@tutamail.com>
2024-09-20 13:39:06 +0200

    Removed redundant boolean checks.

--- gilded_rose.py
+++ gilded_rose.py
@@ -13,12 +13,12 @@ class GildedRose:
             item.sell_in -= 1
             is_past_sell_date = item.sell_in < 0
 
-            # Checking for special items
-            is_aged_brie = name == "Aged Brie"
-            is_backstage_passes = name == "Backstage passes to a TAFKAL80ETC concert"
+            # Special item names
+            aged_brie = "Aged Brie"
+            backstage_passes = "Backstage passes to a TAFKAL80ETC concert"
 
             # Backstage Passes
-            if is_backstage_passes:
+            if name == backstage_passes:
                 if is_past_sell_date:
                     item.quality = 0
                 elif item.sell_in <= 5:
@@ -28,7 +28,7 @@ class GildedRose:
                 else:
                     item.quality += 1
             # Aged Brie
-            elif is_aged_brie:
+            elif name == aged_brie:
                 item.quality += 1
             # Normal item
             elif is_past_sell_date:
